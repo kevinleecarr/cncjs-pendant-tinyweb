be96b07099c99f30f8c3b5a68b0d3f1320b6ab76
kevinleecarr <kevinleecarr@gmail.com>
2017-12-26 14:10:15 -0800

    disable backspace

--- src/app.js
+++ src/app.js
@@ -487,4 +487,18 @@ $(document).keydown(function (e) {
     }
 });
 
+/*
+ * this swallows backspace keys on any non-input element.
+ * stops backspace -> back
+ */
+var rx = /INPUT|SELECT|TEXTAREA/i;
+
+$(document).bind("keydown keypress", function(e){
+    if( e.which == 8 ){ // 8 == backspace
+        if(!rx.test(e.target.tagName) || e.target.disabled || e.target.readOnly ){
+            e.preventDefault();
+        }
+    }
+});
+
 });
